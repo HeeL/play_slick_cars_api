df9f1f2d368849b78f3ce430f3b4a7c9024fe38b
Sergii Paryzhskyi <parizhskiy@gmail.com>
2015-09-23 23:48:10 +0200

    added controller for cars

---
+++ app/controllers/Cars.scala
@@ -0,0 +1,12 @@
+package controllers
+
+import models.Car
+import tables._
+import play.api._
+import play.api.mvc._
+import play.api.Play.current
+
+class Cars extends Controller with CarTable {
+  import driver.api._
+
+}
